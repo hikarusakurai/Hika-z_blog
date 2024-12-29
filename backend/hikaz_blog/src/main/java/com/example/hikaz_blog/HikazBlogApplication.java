a3543831e59c8e5b2b14cbb8246321e0de5ff7ff
hikaru <hika1785040525@gmail.com>
2024-12-29 15:01:23 +0900

    springboot環境構築

---
+++ backend/hikaz_blog/src/main/java/com/example/hikaz_blog/HikazBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.hikaz_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikazBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikazBlogApplication.class, args);
+	}
+
+}
---
+++ backend/hikaz_blog/src/test/java/com/example/hikaz_blog/HikazBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hikaz_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikazBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
